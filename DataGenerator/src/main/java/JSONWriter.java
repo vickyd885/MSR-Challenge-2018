7d1f5056c32f94f8537b9220b98e4ce6c668c57a
vickyd885 <vickyd885@gmail.com>
2017-12-02 11:03:35 +0000

    adds json file writing code using the simple json libary

---
+++ DataGenerator/src/main/java/JSONWriter.java
@@ -0,0 +1,18 @@
+import java.io.FileWriter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JSONWriter{
+
+  public void writeToJSONFile(JSONObject jsonObject, String fileName){
+    try {
+      FileWriter fileWriter = new FileWriter("output/" + fileName + ".json");
+      fileWriter.write(jsonObject.toJSONString());
+      fileWriter.flush();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
