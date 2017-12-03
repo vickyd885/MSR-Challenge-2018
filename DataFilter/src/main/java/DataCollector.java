5b7d36f02457febc93b06625b4c44f334d5e3b12
vickyd885 <vickyd885@gmail.com>
2017-12-03 21:20:48 +0000

    rename the datagenerator folder to datafilter and allow ant to use args for the folder path

---
+++ DataFilter/src/main/java/DataCollector.java
@@ -0,0 +1,126 @@
+/*
+* DataCollector holds the json object, and is responsible for conversions
+* between java types to json types.
+* DataCollector does not invoke the writing to json mechanism, and should be
+* done by the user of DataCollector.
+*/
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Set;
+
+// java time library - this is what kave uses
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+// joda time library
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.Period;
+
+public class DataCollector{
+
+  private JSONObject parent; // Reference to main body
+  private String fileName; // filename that wile be flushed
+  private int entryCount; // reference to latest entry in json
+
+  private DateTime start;
+
+  private TreeMap<ZonedDateTime, JSONObject> tmap;
+
+  private DateTimeFormatter dformat = DateTimeFormatter.ofPattern("HHmm:ss, dd MMM yyyy");
+
+  public DataCollector(String fileName){
+    this.parent = new JSONObject();
+    this.fileName = fileName;
+    this.entryCount = 0;
+    this.start = new DateTime();
+    this.tmap = new TreeMap<ZonedDateTime, JSONObject>();
+  }
+
+  // Adds a number : [...] to help with order
+  public void handleNewEntry(
+    String timeStamp,
+    String eventType,
+    HashMap<String, Object> specificData
+  ){
+    ZonedDateTime zdt = ZonedDateTime.parse(timeStamp);
+
+    this.tmap.put(
+      zdt,
+      addNewEntry(timeStamp, eventType, specificData)
+    );
+
+    ++this.entryCount;
+
+    //System.out.println("Added new entry!");
+  }
+
+  // Creates the actual json object
+  // We deal with the hashmap here
+  public JSONObject addNewEntry(
+    String timeStamp,
+    String eventType,
+    HashMap<String, Object> specificDataMap
+  ){
+    JSONObject newEntry = new JSONObject();
+
+    newEntry.put("time_stamp", timeStamp);
+    newEntry.put("event_type", eventType);
+
+    JSONObject specificDataObj = new JSONObject();
+
+    Set<String> keySet = specificDataMap.keySet();
+
+    for(String key : keySet){
+      specificDataObj.put(key, specificDataMap.get(key));
+    }
+
+    newEntry.put("specific_data", specificDataObj);
+
+    return newEntry;
+  }
+
+
+  public int getNumOfEvents(){
+    return this.entryCount;
+  }
+  // Write all the json data to the file
+  public void flushData(){
+    populateJSONBody();
+    JSONWriter.writeToJSONFile(this.parent, this.fileName);
+  }
+
+  // Prints amount of time passed since DataCollector was initialised
+  public void printTimeInfo(){
+    DateTime now = new DateTime();
+    Interval interval = new Interval(this.start, now);
+    Period period = interval.toPeriod();
+    System.out.println("Time taken for user " + this.fileName +
+      " was ... " + period.getSeconds() +
+      " (seconds)!");
+  }
+
+  public void showAllKeysInTM(){
+    Set<ZonedDateTime> dateSet = this.tmap.keySet();
+    for(ZonedDateTime zdt : dateSet){
+      System.out.println(zdt.format(dformat));
+    }
+  }
+
+  private void populateJSONBody(){
+    Set<ZonedDateTime> dateSet = this.tmap.keySet();
+    int i = 0;
+    for(ZonedDateTime zdt : dateSet){
+      this.parent.put(
+        ++i,
+        this.tmap.get(zdt)
+      );
+    }
+  }
+
+}
