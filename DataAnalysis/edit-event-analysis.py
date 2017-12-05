4e9356f23a2255a6a1432e6c6910783b067ed013
Jasmine Lu <jasminelu.3@gmail.com>
2017-12-05 18:58:37 +0000

    box plot code

--- DataAnalysis/edit-event-analysis.py
+++ DataAnalysis/edit-event-analysis.py
@@ -1,19 +1,27 @@
+import os
 import json
 import numpy as np
 import plotly
 import plotly.graph_objs as go
 
-json_data = open('1.json').read()
-data = json.loads(json_data)
+rootdir = '/Users/jasminelu/Desktop/ToolsCW/DataAnalysis/output'
 
 numberOfChanges = []
 sizeOfChanges = []
 
+def loop_through_files():
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            if file.lower().endswith(('.json')):
+                json_data = open(os.path.join(subdir, file)).read()
+                data = json.loads(json_data)
+                extract_edit_event_data(data)
+
 def extract_edit_event_data(obj):
-    for item in data.values():
+    for item in obj.values():
         if item['event_type'] == 'EditEvent':
             numberOfChanges.append(np.log(item['specific_data']['NumberOfChanges']))
-            sizeOfChanges.append(np.log(item['specific_data']['SizeOfChanges']))
+            sizeOfChanges.append(item['specific_data']['SizeOfChanges'])
 
 def plot_scatter():
     trace = go.Scatter(
@@ -72,7 +80,28 @@ def plot(data, layout, filename):
     fig = go.Figure(data=data, layout=layout)
     plotly.offline.plot(fig, filename=filename)
 
-extract_edit_event_data(data)
-plot_scatter()
-plot_histogram_size_of_changes()
-plot_histogram_number_of_changes()
+def box_plot_size_of_changes():
+    SizeOfChanges = go.Box(
+        x = sizeOfChanges
+    )
+
+    plot_data = [SizeOfChanges]
+
+    layout = go.Layout(
+        font=dict(family='Times', size=16)
+    )
+
+    plot(plot_data, layout, 'box-plot-size')
+
+def box_plot_number_of_changes():
+    NumberOfChanges = go.Box(x=numberOfChanges)
+    plot_data = [NumberOfChanges]
+    plot(plot_data, {}, 'box-plot-number')
+
+
+loop_through_files()
+box_plot_size_of_changes()
+# plot_scatter()
+# plot_histogram_size_of_changes()
+# plot_histogram_number_of_changes()
+# box_plot_number_of_changes()
